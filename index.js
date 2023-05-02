6b376838600507e891d7be8fa386b4aa6798e52d
hridoy281810 <hridoy01724281810@gmail.com>
2023-05-02 15:44:57 +0600

    chef data added this server

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const chefData = require('./data/chefData.json')
 
-app.use(cors());
+app.use(cors())
 
 app.get('/', (req,res)=>{
     res.send('new server running')
+})
+ 
+app.get('/chefData',(req,res)=>{
+    res.send(chefData)
+})
+
+app.get('/chefData/:id', (req,res)=>{
+    const id = req.params.id;
+    const singleChef = chefData.find(chef => chef.id === id)
+    res.send(singleChef)
 })
 
 app.listen(port,()=>{
